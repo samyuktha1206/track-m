ceb8ae7e602471a0be100431f31ca86c6109f12d
Samyuktha <samyuktha.dofficial@gmail.com>
2025-01-21 12:46:32 +0000

    updated application.properties

--- backend/budget-tracker/src/main/java/com/example/budget_tracker/config/SecurityConfig.java
+++ backend/budget-tracker/src/main/java/com/example/budget_tracker/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -35,21 +41,38 @@ public class SecurityConfig {
         return new CorsFilter(source);
     }
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        String username = System.getProperty("SPRING_SCURITY_USER_NAME", "defaulyUser");
+//        String password = System.getProperty("SPRING_SECURITY_USER_PASSWORD", "defaultPassword");
+//
+//        UserDetails user = User.withUsername(username)
+//                .password(passwordEncoder().encode(password))
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors->cors.configurationSource(request -> {
+                .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(Arrays.asList("http://track-m-bucket.s3-website.eu-north-1.amazonaws.com", "http://51.20.85.188"));
                     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    config. setAllowedHeaders(Arrays.asList("*"));
+                    config.setAllowedHeaders(Arrays.asList("*"));
                     config.setAllowCredentials(true);
                     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                     source.registerCorsConfiguration("/**", config);
                     return config;
                 }))
-                .csrf(csrf->csrf.disable())
-                .authorizeHttpRequests(auth-> auth
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/public/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated())
