f8cbe94e03b3d2375bcc48be0f6e512fae9351d6
Samyuktha <samyuktha.dofficial@gmail.com>
2025-01-21 00:43:54 +0000

    added custome spring-security for deployment

--- backend/budget-tracker/src/main/java/com/example/budget_tracker/BudgetTrackerApplication.java
+++ backend/budget-tracker/src/main/java/com/example/budget_tracker/BudgetTrackerApplication.java
@@ -1,5 +1,6 @@
 package com.example.budget_tracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BudgetTrackerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("SPRING_SECURITY_USER_NAME", dotenv.get("SPRING_SECURITY_USER_NAME"));
+		System.setProperty("SPRING_SECURITY_USER_PASSWORD", dotenv.get("SPRING_SECURITY_USER_PASSWORD"));
 		SpringApplication.run(BudgetTrackerApplication.class, args);
 	}
 
